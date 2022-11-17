d6b379a1e30f7adb1fe030eec9f29fcd50105700
Mmesek <13630781+Mmesek@users.noreply.github.com>
2022-11-17 18:32:24 +0100

    Add CLI argument parsing for token & intents

--- source/main.d
+++ source/main.d
@@ -1,7 +1,26 @@
 import gateway;
 
+string token;
+uint intents = 14_271;
+
 void main(string[] args)
 {
+    import std.getopt;
+    import core.stdc.stdlib : exit;
+
+    auto helpInfo = getopt(
+        args,
+        std.getopt.config.required, "token|t", "Bot token to use", &token,
+        "intents|i", "Intents to start bot with", &intents,
+    );
+
+    if (helpInfo.helpWanted)
+    {
+
+        defaultGetoptPrinter("API Client for Discord", helpInfo.options);
+        exit(0);
+    }
+
     auto client = new WS_Client("wss://gateway.discord.gg/?v=10&encoding=json");
-    client.run(args[1], 14_271);
+    client.run(token, intents);
 }
